8c98ce20be24840b6b92bf31b29f7fb166c20c98
Dev Watch <devwatch950@gmail.com>
2021-12-08 15:27:42 -0500

    Made VM accessible by public IP

--- main.tf
+++ main.tf
@@ -33,6 +33,31 @@ resource "azurerm_subnet" "devwatch" {
   address_prefixes = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "devwatch" {
+  name = "publicIP"
+  location = "canadacentral"
+  resource_group_name = azurerm_resource_group.devwatch.name
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "devwatch" {
+  name = "networkSecurityGroup"
+  location = "canadacentral"
+  resource_group_name = azurerm_resource_group.devwatch.name
+
+  security_rule {
+    name = "SSH"
+    priority = 1001
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "22"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "devwatch" {
   name = "devwatch-nic"
   location = azurerm_resource_group.devwatch.location
@@ -42,7 +67,26 @@ resource "azurerm_network_interface" "devwatch" {
     name = "devwatch-subnet"
     subnet_id = azurerm_subnet.devwatch.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.devwatch.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "devwatch" {
+  network_interface_id = azurerm_network_interface.devwatch.id
+  network_security_group_id = azurerm_network_security_group.devwatch.id
+}
+
+# resource "tls_private_key" "sshkey" {
+#   algorithm = "RSA"
+#   rsa_bits = 4096
+# }
+#output "tls_private_key" {
+#  value = tls_private_key.sshkey.private_key_pem
+#  sensitive = true
+#}
+
+output "ip_address" {
+  value = azurerm_public_ip.devwatch.ip_address
 }
 
 resource "azurerm_linux_virtual_machine" "devwatch" {
